d953351ff8025883f2aadb58aa26e628d4e27ffa
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-09-07 05:26:37 +0000

    things

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def whatsthis():
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
-#app.run(host='0.0.0.0', port=8080)
+#app.run(host='0.0.0.0', port=8080) <-- old development flask thing
