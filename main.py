0960496217300aa3b090cfe84679166381a198eb
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-09-07 05:25:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask('app')
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route('/about')
+def about():
+  return render_template("about.html")
+
+@app.route('/projects')
+def projects():
+  return render_template("projects.html")
+
+@app.route('/calc')
+def calc():
+  return render_template("calculator.html")
+
+@app.route('/whatsthis')
+def whatsthis():
+  return render_template("what_is_this.html")
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+#app.run(host='0.0.0.0', port=8080)
---
+++ md.md
@@ -0,0 +1,2 @@
+bg:  https://i.imgur.com/GgIyKKu.jpg
+pfp: https://i.imgur.com/wC8SnIV.png
