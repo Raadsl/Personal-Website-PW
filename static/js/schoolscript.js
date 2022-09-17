79cf60627679efbf8e37fe2c87452aa081166620
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-09-17 12:52:37 +0000

    updated openschool tabs function

--- static/js/schoolscript.js
+++ static/js/schoolscript.js
@@ -1,5 +1,5 @@
 function openschooltabs() {
-  window.open("https://chrlyceumdelft.magister.net/")
-  window.open("https://chrlyceumdelft.itslearning.com/index.aspx")
-  window.open("https://mail.google.com/")
-}
+  window.open("https://chrlyceumdelft.itslearning.com/index.aspx") //ELO
+  window.open("https://mail.google.com/") //mail
+  window.open("https://docs.google.com/") //docs <-- awesome, replitghost writer knew this!!
+  window.open("https://chrlyceumdelft.magister.net/") //magister
