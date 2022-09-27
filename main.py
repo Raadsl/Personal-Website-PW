425a1ca967b906e3dc785a05d49d2813fbeeddb1
Jorik <83394320+Raadsl@users.noreply.github.com>
2022-09-27 16:11:01 +0000

    Added URL a shortener + a project to projects.html

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, jsonify, request,  redirect #flask
+import os #envirin and more
+import string #random strings
+import json #handle json
+import random, time, requests #random thing + time
+from os import environ
+# limit url shorten usage:
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 print('tryna run')
+
 app = Flask('app')
+app.config['JSON_SORT_KEYS'] = False
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    storage_uri="memory://",
+)
 
 
 # ======================= MAIN PAGES =======================
@@ -22,11 +37,11 @@ def projects():
 def whatsthis():
   return render_template("what_is_this.html")
 
-
   
 # ======================= CONTACT =======================
   
 @app.route('/contact')
+@limiter.limit("5/second")
 def contact():
   return render_template(
         '/contact/contact.html',
@@ -60,6 +75,85 @@ def calc():
 def schoolchat():
   return render_template("/school/chat.html")
 
+# ======================= URL SHORTENER =======================
+
+def genID():
+  S = int(5)
+  ran = ''.join(random.choices("-" + string.hexdigits + "-" + string.hexdigits + "-" + string.hexdigits + "-" + string.hexdigits + "-" + string.hexdigits, k = S))
+  id = str(ran)
+  return id
+
+
+
+def saveIDS(id, url):
+    f1 = open(os.getenv('path'), "r")
+    preData = f1.read()
+    f1.close()
+
+    newData = f'"{id}": "{url}"'
+  
+    f3 = open(os.getenv('path'), "w")
+    f3.write(f"{newData},\n{preData}")
+    f3.close()
+  
+    f4 = open(os.getenv('path'), "r")
+    damta = f4.read()
+    f4.close()
+  
+    print(damta)
+
+def getID(id):
+    f = open(os.getenv('path'), "r")
+    data = '{' + f.read() + '}'
+    idsList = json.loads(data)
+    print(str(idsList))
+    f.close()
+    url = f'{idsList[f"{id}"]}'
+    return url
+  
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/redirect')
+def shortenerhome():
+    return render_template("/shortener/redirecthome.html")
+
+@app.route('/r')
+def reditest():
+  return render_template("/shortener/redirecthome.html")
+  
+
+@app.route('/r/dash')
+def dashboard():
+  return render_template("/shortener/redirectdashboard.html")
+  
+  
+@app.route("/r/<id>")
+def urlshortredirect(id):
+    return redirect(getID(id))
+
+@app.route("/create")
+@limiter.limit("5/second")
+def createurl():
+    url = str(request.args.get("url"))
+    if url != "None" and url != "" and url != " ":
+        id = genID()
+        print(id)
+        saveIDS(id, url)
+        data = {
+            "created": "true",
+            "short_url": f"https://raadsel.is-a.dev/r/{id}",
+            "short_url_id": id,
+            "long_url": f"{url}"
+        }
+        return jsonify(data)
+    else:
+        data = {
+            "created": "false",
+            "short_url": "",
+            "short_url_id": "",
+            "long_url": ""
+        }
+        return jsonify(data)
   
 # ======================= ERRORS =======================
   
@@ -78,7 +172,6 @@ def teacupcoffee(e):
 @app.route('/418')
 def foureighteen():
   return render_template("/errors/418.html")
-
 
 # ======================= RUN/START =======================
 
